686d67bf3ad503af26186c873738cf37d55b5740
uumale <malestudio@dingtalk.com>
2022-04-14 10:40:19 +0800

    🎉 init: 初始化gh-pages分支

--- src/common/interface.ts
+++
@@ -1,9 +0,0 @@
-import { SceneConfig } from "@/common/SceneConfig";
-import { OnePart } from "./OnePart";
-/**
- * 模型所有配置的数据类型
- */
-export interface ModelConfigJsonType {
-  sceneConfig: SceneConfig;
-  materialsConfig: OnePart[];
-}
--- src/oasis/utils/io.ts
+++
@@ -1,9 +0,0 @@
-/** 自定义事件名 */
-export class IO {
-  /** 🆗点击模型中的某个部件*/
-  static PICK_PART: string = "pick_part";
-  /** 🆗保存所有配置数据*/
-  static SAVE_ALL_CONFIG: string = "save_all_config";
-  /** 🆗刷新模型*/
-  static REFRESH_MODEL: string = "REFRESH_MODEL";
-}
